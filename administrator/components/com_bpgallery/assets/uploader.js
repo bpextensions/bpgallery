d097509abc3651e73ae67761ad2903bc220a61a1
Artur Stępień <artur.stepien@bestproject.pl>
2017-05-25 02:13:35 +0200

    Change upload images list view to boxes with preview. Added remove functionality with removing from queue list. Moved browse button to drop list. Added a method to recreate drop-down message when there are no files.

--- administrator/components/com_bpgallery/assets/uploader.js
+++ administrator/components/com_bpgallery/assets/uploader.js
@@ -1,83 +1,246 @@
 
 
 
-(function($){
-    
-    $.fn.BPGalleryUpload = function(){
-        
-        this.setupDragAndDrop = function(){
+(function ($) {
+
+    $.fn.BPGalleryUpload = function (settings) {
+
+        // Holds upload settings
+        this.settings = $.extend({
+            'text_intro':'Drag &amp; drop files on this box or select them using <strong>Browse</strong> button below.',
+            'text_browse':'Browse'
+        }, settings);
+
+        // Upload files list (queue)
+        this.queue = [];
+
+        /**
+         * Initialize upload function
+         */
+        this.init = function () {
+
+            // Setup files drag & drop action
+            this.setupDragAndDrop();
+
+            // Setup browse button action
+            this.setupBrowseButtonAction();
+        };
+
+        /**
+         * No more files in the queue, so draw a proper message
+         */
+        this.drawDropContainerIntro = function () {
             
-            // Handle drop in the box
+            // Create contents
+            var $contents = $(
+                '<i class="icon-upload"></i><p>'+this.settings.text_intro+'</p>'+
+                '<button class="btn" id="bpgallery_upload_field_button"><i class="icon-search"></i> '+this.settings.text_browse+'</button>'
+            );
+    
+            
+            // Get drop container element
+            var $container = $('#bpgallery_upload_container');
+            
+            // Draw a drag&drop message
+            $container.text('').append($contents);
+            
+            // Append browser button click action
+            $container.find('#bpgallery_upload_field_button').click(function(){
+                $('#bpgallery_upload_field_input').click();
+            });
+            
+            // Remove class that markes files in container
+            $container.removeClass('hasFiles');
+        };
+
+        // Takes care of creating table with upload queue and progress bar
+        this.prepareFilesList = function () {
+
+            // Get upload container
             var $container = $("#bpgallery_upload_container");
-            $container.on('dragenter', function (e) 
+            $container.addClass('hasFiles');
+
+            // Create the files table
+            var $list = $('#bpgallery_upload_container ul');
+
+            // If queue table doesnt exists
+            if (!$list.length) {
+
+                // Clear container and create files table
+                $container.text('');
+                $list = $('<ul class="images"></ul>');
+                $container.append($list);
+            }
+
+            return $list;
+        };
+
+        /**
+         * Add new files to the list
+         * 
+         * @param   Array   files   A list of File objects to add into table
+         * @returns Null
+         */
+        this.addFiles = function (files) {
+            
+            
+            // Add files to upload queue
+            this.queue.push.apply(this.queue, Array.prototype.slice.call(files));
+            
+            // Update files counter
+            $('#uploadFormLabel small').text('('+this.queue.length+')');
+
+            // Make sure we have the queue table
+            var $list = this.prepareFilesList();
+
+            // Create and append row in queue table for each file
+            for (var i = 0, file = files[0]; i < files.length; i++, file = files[i]) {
+
+                // Create a row
+                var $row = $('<li class="thumbnail pull-left">\n\
+                    <span class="image"></span>\n\
+                    <span class="name">'+file.name+'</span>\n\
+                    <a href="#" class="btn-remove"><i class="icon-trash"></i></a>\n\
+                </li>');
+
+                // Prepare file remove action
+                $row.find('.btn-remove').click($.proxy(this.removeFileAction, this));
+                
+                // Get preview element
+                file.$preview = $row.find('.image');
+                
+                // Load preview image
+                file.$preview.reader = new FileReader();
+                $(file.$preview.reader).on('load',$.proxy(function (e) {
+                    this.css('background-image', 'url('+e.target.result+')');
+                }, file.$preview));
+                file.$preview.reader.readAsDataURL(file);
+
+                // Append a row
+                $list.append($row);
+            }
+
+        };
+
+        /**
+         * Remove single file from upload queue and queue table.
+         * 
+         * @param Event e
+         */
+        this.removeFileAction = function (e) {
+
+            // Get remove button element
+            var $target = $(e.target);
+            var $row = $target.closest('li');
+            var index = $row.parent().children().index($row);
+            
+            // Remove file from queue list
+            this.queue.splice(index,1);
+            
+            // Remove file row in queue table
+            $row.remove();
+            
+             // Update files counter
+            $('#uploadFormLabel small').text('('+this.queue.length+')');
+            
+            // If there are no more files in the queue
+            if( this.queue.length<1 ) {
+                
+                // Create intro text on drop container
+                this.drawDropContainerIntro();
+                
+                // Remove images counter
+                $('#uploadFormLabel small').text('');
+            }
+        };
+
+        /**
+         * Setups a Drag & Drop functionality.
+         * 
+         * @returns Null
+         */
+        this.setupDragAndDrop = function () {
+
+            // Get files upload container element
+            var $container = $("#bpgallery_upload_container");
+
+            // Handle drop in the box
+            $container.on('dragenter', function (e)
             {
                 e.stopPropagation();
                 e.preventDefault();
                 $container.addClass('hover');
             });
-            $container.on('dragover', function (e) 
-            {
-                 e.stopPropagation();
-                 e.preventDefault();
-            });
-            $container.on('dragleave', function (e) 
-            {
-              $container.removeClass('hover');
-              $container.removeClass('active');
-            });
-            $container.on('drop', function (e) 
-            {
-
-                 $container.addClass('hasFiles');
-                 e.preventDefault();
-                 var files = e.originalEvent.dataTransfer.files;
-                 
-                 // Display the list of files
-                 $table = $('#bpgallery_upload_container table');
-                 if( !$table ) {
-                     $container.html('');
-                     $table = $('<table class="table table-stripped"></table>');
-                     $container.append($table);
-                 }
-                 
-//                console.log(files);
-                 files.forEach(function(file){
-//                    console.log(file);
-                     
-                 });
-                 file
-                 for(var i; i<files.length;i++) {
-                     $row = $('<tr><td>'+file.name+'</td><td>'+Math.round(file.size/1024)+' MB</td><td></td></tr>');
-                 }
-
-                 //We need to send dropped files to Server
-//                 handleFileUpload(files,$container);
-            });
-            
-            // handle drop outside of the box
-            $(document).on('dragenter', function (e) 
+            $container.on('dragover', function (e)
             {
                 e.stopPropagation();
                 e.preventDefault();
             });
-            $(document).on('dragover', function (e) 
+            $container.on('dragleave', function (e)
             {
-              e.stopPropagation();
-              e.preventDefault();
-//              $container.addClass('hover');
+                $container.removeClass('hover');
+                $container.removeClass('active');
             });
-            $(document).on('drop', function (e) 
+            $container.on('drop', $.proxy(function (e)
+            {
+                e.stopPropagation();
+                e.preventDefault();
+
+                // Create FileList object
+                var files = e.originalEvent.dataTransfer.files;
+
+                // Add selected files to queue table
+                this.addFiles(files);
+
+            }, this));
+
+            // handle drop outside of the box
+            $(document).on('dragenter', function (e)
+            {
+                e.stopPropagation();
+                e.preventDefault();
+            });
+            $(document).on('dragover', function (e)
+            {
+                e.stopPropagation();
+                e.preventDefault();
+            });
+            $(document).on('drop', function (e)
             {
                 e.stopPropagation();
                 e.preventDefault();
             });
         };
-        
-        this.setupDragAndDrop();
-        
-        console.log('Uploader setup done.')
-        
+
+        /**
+         * Setups a Browse button functionality.
+         * 
+         * @returns Null
+         */
+        this.setupBrowseButtonAction = function () {
+
+            // Browse button action
+            $('#bpgallery_upload_field_button').click($.proxy(function () {
+
+
+                // Fire click event on files browse input
+                $('#bpgallery_upload_field_input').click();
+
+            }, this));
+
+            // Files select action
+            $('#bpgallery_upload_field_input').change($.proxy(function (e) {
+
+                // Render and add selected files to the queue
+                this.addFiles(e.target.files);
+
+            }, this));
+        };
+
+        // Initialize uploader
+        this.init();
+
         return this;
     };
-    
+
 })(jQuery);
--- administrator/components/com_bpgallery/views/images/tmpl/default_upload.php
+++ administrator/components/com_bpgallery/views/images/tmpl/default_upload.php
@@ -1,38 +1,49 @@
 <?php
 defined('_JEXEC') or die;
 
-$this->document->addScript('/administrator/components/com_bpgallery/assets/uploader.js');
+$this->document->addScriptVersion(
+    '/administrator/components/com_bpgallery/assets/uploader.js',
+    filemtime(JPATH_COMPONENT.'/assets/uploader.js'
+));
 $this->document->addScriptDeclaration('
 	jQuery(document).ready(function($){
 		$("#toolbar-new button").attr("onclick","jQuery(\"#bpgallery_upload_form\").modal(\"show\")");
-		$(document).BPGalleryUpload();
+		$(document).BPGalleryUpload({
+            text_intro:"'.JText::_('COM_BPGALLERY_IMAGES_UPLOAD_TIP').'",
+            text_browse:"'.JText::_('COM_BPGALLERY_IMAGES_BROWSE_BUTTON').'",
+        });
 	});
 ');
-$this->document->addStyleSheet('/administrator/components/com_bpgallery/assets/component.css');
-
+$this->document->addStyleSheetVersion('/administrator/components/com_bpgallery/assets/component.css',
+    filemtime(JPATH_COMPONENT.'/assets/component.css'));
 ?>
 <!-- Modal -->
 <div id="bpgallery_upload_form" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="bpgallery_upload_form" aria-hidden="true">
-  <div class="modal-header">
-    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-    <h3 id="uploadFormLabel"><?php echo JText::_('COM_BPGALLERY_IMAGES_UPLOAD_HEADER') ?></h3>
-  </div>
-  <div class="modal-body">
-	  <div id="bpgallery_upload_container">
-		  <i class="icon-upload"></i>
-		  <p>Drag & drop files on this box or <a href="#" class="btn btn-default btn-small">select them</a> from your computer.</p>
-	  </div>
-  </div>
-  <div class="modal-footer">
-	<?php
-	$field = new JFormFieldCategory();
-	$xml = new SimpleXMLElement('<field name="category" type="category" extension="com_bpgallery"><option value="">JOPTION_SELECT_CATEGORY</option></field>');
-	$field->setup($xml, '');
-	?>
-	<div class="pull-left">
-	  <?php echo $field->renderField(array('hiddenLabel'=>true)) ?>
-	</div>
-    <button class="btn" data-dismiss="modal" aria-hidden="true"><?php echo JText::_('JCANCEL') ?></button>
-    <button class="btn btn-primary"><i class="icon-upload"></i> <?php echo JText::_('COM_BPGALLERY_IMAGES_UPLOAD_BUTTON') ?></button>
-  </div>
+    <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+        <h3 id="uploadFormLabel"><?php echo JText::_('COM_BPGALLERY_IMAGES_UPLOAD_HEADER') ?><small></small></h3>
+    </div>
+    <div class="modal-body">
+        <div id="bpgallery_upload_container">
+            <i class="icon-upload"></i>
+            <p><?php echo JText::_('COM_BPGALLERY_IMAGES_UPLOAD_TIP') ?></p>
+            <p><button class="btn" id="bpgallery_upload_field_button"><i class="icon-search"></i> <?php echo JText::_('COM_BPGALLERY_IMAGES_BROWSE_BUTTON') ?></button></p>
+        </div>
+    </div>
+    <div class="modal-footer">
+        <?php
+        $field = new JFormFieldCategory();
+        $xml   = new SimpleXMLElement('<field name="category" type="category" extension="com_bpgallery"><option value="">JOPTION_SELECT_CATEGORY</option></field>');
+        $field->setup($xml, '');
+        $field->setValue($this->state->get('filter.category_id',''));
+        ?>
+        <div class="pull-left">
+            <?php echo $field->renderField(array('hiddenLabel' => true)) ?>
+        </div>
+        <input name="bpgallery_upload_field_input" type="file" id="bpgallery_upload_field_input" multiple class="hidden"/>
+        <div class="btn-group">
+            <button class="btn" data-dismiss="modal" aria-hidden="true"><?php echo JText::_('JCANCEL') ?></button>
+            <button class="btn btn-primary"><i class="icon-upload"></i> <?php echo JText::_('COM_BPGALLERY_IMAGES_UPLOAD_BUTTON') ?></button>
+        </div>
+    </div>
 </div>
