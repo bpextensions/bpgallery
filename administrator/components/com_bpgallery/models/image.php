468210f62e0c26eb7a355599c9dc8ebdfe1f6ba9
Artur Stępień <artur.stepien@bestproject.pl>
2017-06-03 18:27:18 +0200

    Image upload methods.

--- administrator/components/com_bpgallery/models/image.php
+++ administrator/components/com_bpgallery/models/image.php
@@ -19,6 +19,17 @@ class BPGalleryModelImage extends JModelAdmin
      */
     public $typeAlias = 'com_bpgallery.image';
 
+    public function __construct($config = array())
+    {
+
+        // Base object construction
+        parent::__construct($config);
+
+        // Store basic params into model for laster use
+        $this->params      = JComponentHelper::getParams('com_bpgallery');
+        $this->images_base = $this->params->get('images_path', '/images/gallery');
+    }
+
     /**
      * Method to test whether a record can be deleted.
      *
@@ -137,6 +148,170 @@ class BPGalleryModelImage extends JModelAdmin
      */
     protected function prepareTable($table)
     {
-        $table->name = htmlspecialchars_decode($table->name, ENT_QUOTES);
+        $table->title = htmlspecialchars_decode($table->get('title'), ENT_QUOTES);
+    }
+
+    /**
+     * Saves data.
+     * 
+     * @param   Array   $data   Data to save.
+     * 
+     * @return  boolean
+     */
+    public function save($data)
+    {
+
+        // If there is a file to upload and upload failed
+        if (isset($data['upload_image']) AND ! $this->uploadFile($data)) {
+            return false;
+        }
+
+        // Upload was success or there was nothing to upload, so just save the data
+        // and return the result.
+        return parent::save($data);
+    }
+
+    /**
+     *
+     * @param   Array   $data
+     * 
+     * @return  boolean
+     */
+    protected function uploadFile(&$data)
+    {
+
+        // Get upload file details
+        $basename = pathinfo($data['upload_file_name'], PATHINFO_BASENAME);
+        $ext      = pathinfo($data['upload_file_name'], PATHINFO_EXTENSION);
+
+        // Fill the title if it is missing
+        if (empty($data['title'])) {
+            $data['title'] = $basename;
+        }
+
+        // Prepare path
+        $filename = $this->getSafeFilename($basename, $ext);
+        $path     = JPATH_ROOT.$this->images_path.'/'.$filename;
+
+        // If uploading the file failed.
+        if (!JFile::upload($data['upload_image'], $path)) {
+
+            // Return failure.
+            return false;
+
+            // Uplod successed, so save filename to image data.
+        } else {
+
+            $data['image'] = $filename;
+        }
+
+        // Generate thumbnails
+        // If data save opr generating thumbnails failes
+        if (!$this->generateThumbnails($path) OR ! parent::save($data)) {
+
+            // Remove thumbnails
+            $this->removeThumbnails($path);
+
+            // Return failure
+            return false;
+        }
+
+        // All fine, return success
+        return true;
+    }
+
+    /**
+     * Get a unique filename from a provided filename using recurency.
+     *
+     * @param   String  $basename   File basename.
+     * @param   String  $extension  File extension.
+     * 
+     * @return  String
+     */
+    protected function getSafeFilename($basename, $extension)
+    {
+
+        // Prepare path for this filename
+        $filename = $basename.'.'.strtolower($extension);
+        $path     = $this->images_path.'/'.$filename;
+
+        // If path is save (no overwriting)
+        if (!file_exists($path)) {
+
+            // Return this
+            return $filename;
+        } else {
+
+            // Create new basename
+            $parts = explode('-', $basename);
+
+            // Get string after last -
+            $end = end($parts);
+
+            // If this is a number
+            if (is_numeric($end)) {
+
+                // Remove it
+                array_pop($parts);
+
+                // increase it and append again
+                $parts[] = $end++;
+
+                // Not a number
+            } else {
+
+                // So add 2nd version number
+                $parts [] = 2;
+            }
+
+            // Create a basename
+            $basename = implode('-', $parts);
+
+            // Check the new filename
+            return $this->getSafeFilename($basename, $extension);
+        }
+    }
+
+    /**
+     * Try to remove all thumbnails for the selected file.
+     * 
+     * @param   String  $path   Path to the original image file.
+     *
+     * @return  boolean
+     */
+    protected function removeThumbnails($path)
+    {
+
+        $filename = pathinfo($path, PATHINFO_FILENAME);
+
+        // TODO: Remove thumbnails
+        return true;
+    }
+
+    /**
+     * Generate thumbnails for the provided file.
+     *
+     * @param   String  $path   Patht to the image.
+     * 
+     * @return  boolean
+     */
+    protected function generateThumbnails($path)
+    {
+        // TODO: Generate thumbnails.
+        return true;
+    }
+
+    /**
+     * Method to allow derived classes to preprocess the data.
+     *
+     * @param   string  $context  The context identifier.
+     * @param   mixed   &$data    The data to be processed. It gets altered directly.
+     * @param   string  $group    The name of the plugin group to import (defaults to "content").
+     *
+     * @return  void
+     */
+    protected function preprocessData($context, &$data, $group = 'bpgallery')
+    {
+        parent::preprocessData($context, $data, $group);
     }
 }
