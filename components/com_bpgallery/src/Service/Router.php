7a628c0698be43da3952aa07ca313351cc78efce
Artur Stępień <artur.stepien@bestproject.pl>
2024-08-25 15:53:44 +0200

    Prepare base router service for Joomla! 5

---
+++ components/com_bpgallery/src/Service/Router.php
@@ -0,0 +1,277 @@
+<?php
+
+/**
+ * @author            ${author.name} (${author.email})
+ * @website           ${author.url}
+ * @copyright         ${copyrights}
+ * @license           ${license.url} ${license.name}
+ * @package           ${package}
+ * @subpackage        ${subpackage}
+ */
+
+namespace BPExtensions\Component\BPGallery\Site\Service;
+
+\defined('_JEXEC') or die;
+
+use Joomla\CMS\Application\SiteApplication;
+use Joomla\CMS\Categories\CategoryFactoryInterface;
+use Joomla\CMS\Categories\CategoryInterface;
+use Joomla\CMS\Component\ComponentHelper;
+use Joomla\CMS\Component\Router\RouterView;
+use Joomla\CMS\Component\Router\RouterViewConfiguration;
+use Joomla\CMS\Component\Router\Rules\MenuRules;
+use Joomla\CMS\Component\Router\Rules\NomenuRules;
+use Joomla\CMS\Component\Router\Rules\StandardRules;
+use Joomla\CMS\Menu\AbstractMenu;
+use Joomla\Database\DatabaseInterface;
+use Joomla\Database\ParameterType;
+
+/**
+ * Routing class of com_bpgallery
+ */
+class Router extends RouterView
+{
+    /**
+     * Flag to remove IDs
+     *
+     * @var    boolean
+     */
+    protected $noIDs = false;
+
+    /**
+     * The category factory
+     *
+     * @var CategoryFactoryInterface
+     */
+    private $categoryFactory;
+
+    /**
+     * The category cache
+     *
+     * @var  array
+     */
+    private $categoryCache = [];
+
+    /**
+     * The db
+     *
+     * @var DatabaseInterface
+     */
+    private $db;
+
+    /**
+     * BP Gallery Component router constructor
+     *
+     * @param   SiteApplication           $app              The application object
+     * @param   AbstractMenu              $menu             The menu object to work with
+     * @param   CategoryFactoryInterface  $categoryFactory  The category object
+     * @param   DatabaseInterface         $db               The database object
+     */
+    public function __construct(
+        SiteApplication $app,
+        AbstractMenu $menu,
+        CategoryFactoryInterface $categoryFactory,
+        DatabaseInterface $db
+    ) {
+        $this->categoryFactory = $categoryFactory;
+        $this->db              = $db;
+
+        $params      = ComponentHelper::getParams('com_bpgallery');
+        $this->noIDs = (bool)$params->get('sef_ids');
+        $categories  = new RouterViewConfiguration('categories');
+        $categories->setKey('id');
+        $this->registerView($categories);
+        $category = new RouterViewConfiguration('category');
+        $category->setKey('id')->setParent($categories, 'catid')->setNestable()->addLayout('default');
+        $this->registerView($category);
+        $image = new RouterViewConfiguration('image');
+        $image->setKey('id')->setParent($category, 'catid');
+        $this->registerView($image);
+        $form = new RouterViewConfiguration('form');
+        $form->setKey('a_id');
+        $this->registerView($form);
+
+        parent::__construct($app, $menu);
+
+        $this->attachRule(new MenuRules($this));
+        $this->attachRule(new StandardRules($this));
+        $this->attachRule(new NomenuRules($this));
+    }
+
+    /**
+     * Method to get the segment(s) for a category
+     *
+     * @param   string  $id     ID of the category to retrieve the segments for
+     * @param   array   $query  The request that is built right now
+     *
+     * @return  array|string  The segments of this item
+     */
+    public function getCategoriesSegment($id, $query)
+    {
+        return $this->getCategorySegment($id, $query);
+    }
+
+    /**
+     * Method to get the segment(s) for a category
+     *
+     * @param   string  $id     ID of the category to retrieve the segments for
+     * @param   array   $query  The request that is built right now
+     *
+     * @return  array|string  The segments of this item
+     */
+    public function getCategorySegment($id, $query)
+    {
+        $category = $this->getCategories(['access' => true])->get($id);
+
+        if ($category) {
+            $path    = array_reverse($category->getPath(), true);
+            $path[0] = '1:root';
+
+            if ($this->noIDs) {
+                foreach ($path as &$segment) {
+                    [$id, $segment] = explode(':', $segment, 2);
+                }
+            }
+
+            return $path;
+        }
+
+        return [];
+    }
+
+    /**
+     * Method to get categories from cache
+     *
+     * @param   array  $options  The options for retrieving categories
+     *
+     * @return  CategoryInterface  The object containing categories
+     */
+    private function getCategories(array $options = []): CategoryInterface
+    {
+        $key = serialize($options);
+
+        if (!isset($this->categoryCache[$key])) {
+            $this->categoryCache[$key] = $this->categoryFactory->createCategory($options);
+        }
+
+        return $this->categoryCache[$key];
+    }
+
+    /**
+     * Method to get the segment(s) for a form
+     *
+     * @param   string  $id     ID of the image form to retrieve the segments for
+     * @param   array   $query  The request that is built right now
+     *
+     * @return  array|string  The segments of this item
+     */
+    public function getFormSegment($id, $query)
+    {
+        return $this->getImageSegment($id, $query);
+    }
+
+    /**
+     * Method to get the segment(s) for an image
+     *
+     * @param   string  $id     ID of the image to retrieve the segments for
+     * @param   array   $query  The request that is built right now
+     *
+     * @return  array|string  The segments of this item
+     */
+    public function getImageSegment($id, $query)
+    {
+        if (!strpos($id, ':')) {
+            $id      = (int)$id;
+            $dbquery = $this->db->getQuery(true);
+            $dbquery->select($this->db->quoteName('alias'))
+                ->from($this->db->quoteName('#__bpgallery_images'))
+                ->where($this->db->quoteName('id') . ' = :id')
+                ->bind(':id', $id, ParameterType::INTEGER);
+            $this->db->setQuery($dbquery);
+
+            $id .= ':' . $this->db->loadResult();
+        }
+
+        if ($this->noIDs) {
+            [$void, $segment] = explode(':', $id, 2);
+
+            return [$void => $segment];
+        }
+
+        return [(int)$id => $id];
+    }
+
+    /**
+     * Method to get the segment(s) for a category
+     *
+     * @param   string  $segment  Segment to retrieve the ID for
+     * @param   array   $query    The request that is parsed right now
+     *
+     * @return  mixed   The id of this item or false
+     */
+    public function getCategoriesId($segment, $query)
+    {
+        return $this->getCategoryId($segment, $query);
+    }
+
+    /**
+     * Method to get the id for a category
+     *
+     * @param   string  $segment  Segment to retrieve the ID for
+     * @param   array   $query    The request that is parsed right now
+     *
+     * @return  mixed   The id of this item or false
+     */
+    public function getCategoryId($segment, $query)
+    {
+        if (isset($query['id'])) {
+            $category = $this->getCategories(['access' => false])->get($query['id']);
+
+            if ($category) {
+                foreach ($category->getChildren() as $child) {
+                    if ($this->noIDs) {
+                        if ($child->alias == $segment) {
+                            return $child->id;
+                        }
+                    } else {
+                        if ($child->id == (int)$segment) {
+                            return $child->id;
+                        }
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Method to get the segment(s) for an image
+     *
+     * @param   string  $segment  Segment of the image to retrieve the ID for
+     * @param   array   $query    The request that is parsed right now
+     *
+     * @return  mixed   The id of this item or false
+     */
+    public function getImageId($segment, $query)
+    {
+        if ($this->noIDs) {
+            $dbquery = $this->db->getQuery(true);
+            $dbquery->select($this->db->quoteName('id'))
+                ->from($this->db->quoteName('##__bpgallery_images'))
+                ->where(
+                    [
+                        $this->db->quoteName('alias') . ' = :alias',
+                        $this->db->quoteName('catid') . ' = :catid',
+                    ]
+                )
+                ->bind(':alias', $segment)
+                ->bind(':catid', $query['id'], ParameterType::INTEGER);
+            $this->db->setQuery($dbquery);
+
+            return (int)$this->db->loadResult();
+        }
+
+        return (int)$segment;
+    }
+}
