7e093e6d104e847c119e5443b972ed913be87f7d
Artur Stępień <artur.stepien@bestproject.pl>
2024-08-25 14:22:42 +0200

    Creating ImagePrepare event for plugins integration.

---
+++ administrator/components/com_bpgallery/src/Event/ImagePrepareEvent.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace BPExtensions\Component\BPGallery\Administrator\Event;
+
+use Joomla\CMS\Event\AbstractImmutableEvent;
+use Joomla\Registry\Registry;
+
+// phpcs:disable PSR1.Files.SideEffects
+\defined('_JEXEC') or die;
+// phpcs:enable PSR1.Files.SideEffects
+
+/**
+ * Class for BPGallery event.
+ * Example:
+ *  new ImagePrepareEvent('onEventName', ['context' => 'com_example.example', 'subject' => $contentObject, 'params' => $params]);
+ */
+class ImagePrepareEvent extends AbstractImmutableEvent
+{
+
+    /**
+     * Getter for the item argument.
+     *
+     * @return  object
+     */
+    public function getItem(): object
+    {
+        return $this->arguments['subject'];
+    }
+
+    /**
+     * Getter for the item argument.
+     *
+     * @return  Registry
+     */
+    public function getParams(): Registry
+    {
+        return $this->arguments['params'];
+    }
+
+    /**
+     * Setter for the subject argument.
+     *
+     * @param   object  $value  The value to set
+     *
+     * @return  object
+     */
+    protected function onSetSubject(object $value): object
+    {
+        return $value;
+    }
+
+    /**
+     * Setter for the params argument.
+     *
+     * @param   Registry  $value  The value to set
+     *
+     * @return  Registry
+     */
+    protected function onSetParams($value): Registry
+    {
+        // This is for b/c compatibility, because some extensions pass a mixed types
+        if (!$value instanceof Registry) {
+            $value = new Registry($value);
+
+            // @TODO: In 6.0 throw an exception
+            @trigger_error(
+                sprintf('The "params" attribute for the event "%s" must be type of Registry. In 6.0 it will throw an exception',
+                    $this->getName()),
+                E_USER_DEPRECATED
+            );
+        }
+
+        return $value;
+    }
+}
