42f06f2d2497f6ece9b9852f1e842a3405cff430
Artur Stępień <artur.stepien@bestproject.pl>
2024-09-03 18:17:31 +0200

    Admin templates

---
+++ administrator/components/com_bpgallery/tmpl/images/default_upload.php
@@ -0,0 +1,93 @@
+<?php
+
+/**
+ * @author            ${author.name} (${author.email})
+ * @website           ${author.url}
+ * @copyright         ${copyrights}
+ * @license           ${license.url} ${license.name}
+ * @package           ${package}
+ * @subpackage        ${subpackage}
+ */
+
+use Joomla\CMS\Factory;
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\Router\Route;
+
+defined('_JEXEC') or die;
+
+$doc = Factory::getDocument();
+
+JLoader::register('JFormFieldModal_Category',
+    JPATH_ADMINISTRATOR . '/components/com_categories/models/fields/modal/category.php');
+
+Text::script('COM_BPGALLERY_IMAGES_UPLOAD_TIP');
+Text::script('COM_BPGALLERY_IMAGES_BROWSE_BUTTON');
+Text::script('COM_BPGALLERY_IMAGES_BTN_ADD_LABEL');
+
+BPGalleryHelper::includeEntryPointAssets('uploader');
+$options = [
+    'url' => Route::_('index.php?option=com_bpgallery&task=image.upload&format=json'),
+];
+$options = json_encode($options);
+$doc->addScriptDeclaration("
+	jQuery(document).ready(function($){
+	
+	    // Show upload window
+	    var showUploadWindow = function(e){
+	        e.stopPropagation();
+	        $('#bpgallery_upload_form').modal('show');
+	    };
+	
+	    // Bind New button click event
+		$('#toolbar-new button').removeAttr('onclick').click(showUploadWindow);
+		
+		$(document).BPGalleryUpload($options);
+	});
+");
+?>
+<!-- Modal -->
+<div id="bpgallery_upload_form" class="modal hide fade" tabindex="-1" role="dialog"
+     aria-labelledby="bpgallery_upload_form" aria-hidden="true">
+    <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+        <h3 id="uploadFormLabel"><?php echo JText::_('COM_BPGALLERY_IMAGES_UPLOAD_HEADER') ?><small></small></h3>
+    </div>
+    <div class="modal-body">
+        <div id="bpgallery_upload_container">
+            <i class="icon-upload"></i>
+            <p><?php echo JText::_('COM_BPGALLERY_IMAGES_UPLOAD_TIP') ?></p>
+            <p>
+                <button class="btn btn-success" id="bpgallery_upload_field_button"><i
+                            class="icon-search"></i> <?php echo JText::_('COM_BPGALLERY_IMAGES_BROWSE_BUTTON') ?>
+                </button>
+            </p>
+        </div>
+    </div>
+    <div class="modal-footer">
+        <?php
+        $field = new JFormFieldModal_Category();
+        $xml   = new SimpleXMLElement('<field name="category" type="modal_category" extension="com_bpgallery" new="true" select="true" />');
+        $field->setup($xml, '');
+        $field->setValue($this->state->get('filter.category_id', ''));
+        ?>
+        <div class="pull-left category-selection">
+            <?php echo str_ireplace('type="hidden"', 'type="text" class="hidden"',
+                $field->renderField(['hiddenLabel' => true])) ?>
+        </div>
+        <input name="bpgallery_upload_field_input" type="file" id="bpgallery_upload_field_input" multiple
+               class="hidden"/>
+        <div class="pull-right d-flex align-items-center">
+            <div class="mr-2" id="bpgallery_upload_missing_params_warning">
+                <p class="m-0"><i class="icon-warning mr-2"
+                                  aria-hidden="true"></i> <?php echo Text::_('COM_BPGALLERY_UPLOAD_MISSING_PARAMS_WARNING') ?>
+                </p>
+            </div>
+            <div class="btn-group">
+                <button class="btn" data-dismiss="modal" aria-hidden="true"><?php echo JText::_('JCANCEL') ?></button>
+                <button class="btn btn-primary" disabled><i
+                            class="icon-upload"></i> <?php echo JText::_('COM_BPGALLERY_IMAGES_UPLOAD_BUTTON') ?>
+                </button>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ administrator/components/com_bpgallery/tmpl/images/modal.php
@@ -0,0 +1,186 @@
+<?php
+/**
+ * @package     Joomla.Administrator
+ * @subpackage  com_bpgallery
+ *
+ * @copyright   Copyright (C) 2005 - 2019 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+
+$app = JFactory::getApplication();
+
+if ($app->isClient('site')) {
+    JSession::checkToken('get') or die(JText::_('JINVALID_TOKEN'));
+}
+
+JLoader::register('BPGalleryHelperRoute', JPATH_ADMINISTRATOR . '/components/com_bpgallery/helpers/route.php');
+
+JHtml::addIncludePath(JPATH_COMPONENT . '/helpers/html');
+
+JHtml::_('behavior.core');
+JHtml::_('bootstrap.tooltip', '.hasTooltip', ['placement' => 'bottom']);
+JHtml::_('bootstrap.popover', '.hasPopover', ['placement' => 'right']);
+JHtml::_('formbehavior.chosen', 'select');
+JHtml::_('behavior.polyfill', ['event'], 'lt IE 9');
+JHtml::_('script', 'com_bpgallery/modal_image.js', ['version' => 'auto', 'relative' => true]);
+
+// Special case for the search field tooltip.
+$searchFilterDesc = $this->filterForm->getFieldAttribute('search', 'description', null, 'filter');
+JHtml::_('bootstrap.tooltip', '#filter_search', ['title' => JText::_($searchFilterDesc), 'placement' => 'bottom']);
+
+$function  = $app->input->getCmd('function', 'jSelectBPGalleryImage');
+$editor    = $app->input->getCmd('editor', '');
+$listOrder = $this->escape($this->state->get('list.ordering'));
+$listDirn  = $this->escape($this->state->get('list.direction'));
+$onclick   = $this->escape($function);
+
+if (!empty($editor)) {
+    // This view is used also in com_menus. Load the xtd script only if the editor is set!
+//	JFactory::getDocument()->addScriptOptions('xtd-bpgalleryimage', array('editor' => $editor));
+    $onclick = "jSelectBPGalleryImage";
+}
+
+BPGalleryHelper::includeEntryPointAssets('component');
+?>
+<div class="container-popup">
+
+    <form
+            action="<?php echo JRoute::_('index.php?option=com_bpgallery&view=images&layout=modal&tmpl=component&editor=' . $editor . '&function=' . $function . '&' . JSession::getFormToken() . '=1'); ?>"
+            method="post" name="adminForm" id="adminForm" class="form-inline">
+
+        <?php echo JLayoutHelper::render('joomla.searchtools.default', ['view' => $this]); ?>
+
+        <?php if (empty($this->items)) : ?>
+            <div class="alert alert-no-items">
+                <?php echo JText::_('JGLOBAL_NO_MATCHING_RESULTS'); ?>
+            </div>
+        <?php else : ?>
+            <table class="table table-striped table-condensed">
+                <thead>
+                <tr>
+                    <th width="1%" class="nowrap center">
+                        <?php echo JHtml::_('searchtools.sort', 'JSTATUS', 'a.state', $listDirn, $listOrder); ?>
+                    </th>
+                    <th class="nowrap hidden-phone">
+                        <?php echo JHtml::_('searchtools.sort', 'COM_BPGALLERY_HEADING_TITLE', 'a.title', $listDirn,
+                            $listOrder); ?>
+                    </th>
+                    <th width="10%" class="nowrap hidden-phone">
+                        <?php echo JHtml::_('searchtools.sort', 'JGRID_HEADING_ACCESS', 'access_level', $listDirn,
+                            $listOrder); ?>
+                    </th>
+                    <th width="10%" class="nowrap hidden-phone">
+                        <?php echo JHtml::_('searchtools.sort', 'JGRID_HEADING_LANGUAGE', 'a.language', $listDirn,
+                            $listOrder); ?>
+                    </th>
+                    <th width="1%" class="nowrap hidden-phone">
+                        <?php echo JHtml::_('searchtools.sort', 'JGRID_HEADING_ID', 'a.id', $listDirn, $listOrder); ?>
+                    </th>
+                </tr>
+                </thead>
+                <tfoot>
+                <tr>
+                    <td colspan="6">
+                        <?php echo $this->pagination->getListFooter(); ?>
+                    </td>
+                </tr>
+                </tfoot>
+                <tbody>
+                <?php
+                $iconStates = [
+                    -2 => 'icon-trash',
+                    0  => 'icon-unpublish',
+                    1  => 'icon-publish',
+                    2  => 'icon-archive',
+                ];
+                ?>
+                <?php foreach ($this->items as $i => $item) :
+                    $ordering = ($listOrder == 'ordering');
+                    $item->cat_link = JRoute::_('index.php?option=com_categories&extension=com_bpgallery&task=edit&type=other&cid[]=' . $item->catid);
+                    $item->item_link = JRoute::_('index.php?option=com_bpgallery&task=image.edit&id=' . (int)$item->id);
+                    $item->thumbnail = BPGalleryHelper::getThumbnail($item, 64, 64, BPGalleryHelper::METHOD_CROP);
+                    $item->thumbnail_preview = BPGalleryHelper::getThumbnail($item, 320, 320,
+                        BPGalleryHelper::METHOD_FIT);
+                    ?>
+                    <?php if ($item->language && JLanguageMultilang::isEnabled()) {
+                    $tag = strlen($item->language);
+                    if ($tag == 5) {
+                        $lang = substr($item->language, 0, 2);
+                    } elseif ($tag == 6) {
+                        $lang = substr($item->language, 0, 3);
+                    } else {
+                        $lang = '';
+                    }
+                } elseif (!JLanguageMultilang::isEnabled()) {
+                    $lang = '';
+                }
+                    ?>
+                    <tr class="row<?php echo $i % 2; ?>" sortable-group-id="<?php echo $item->catid; ?>">
+                        <td class="center" style="vertical-align: middle">
+                            <span class="<?php echo $iconStates[$this->escape($item->state)]; ?>"
+                                  aria-hidden="true"></span>
+                        </td>
+                        <td class="nowrap has-context" style="vertical-align: middle">
+                            <div class="pull-left">
+                                <a class="select-link thumbnail hasPopover" href="javascript:void(0)"
+                                   data-function="<?php echo $this->escape($onclick); ?>"
+                                   data-id="<?php echo $item->id; ?>"
+                                   data-title="<?php echo $this->escape($item->title); ?>"
+                                   data-uri="<?php echo $this->escape(BPGalleryHelperRoute::getImageRoute($item->id,
+                                       $item->language)); ?>"
+                                   data-language="<?php echo $this->escape($lang); ?>" data-placement="right"
+                                   data-content="<img src='<?php echo $item->thumbnail_preview ?>' />"
+                                   data-original-title="<?php echo $this->escape($item->title) ?>">
+                                    <img src="<?php echo $item->thumbnail ?>"
+                                         alt="<?php echo $this->escape($item->title) ?>"/>
+                                </a>
+                            </div>
+                            <div class="pull-left">
+                                <a class="select-link" href="javascript:void(0)"
+                                   data-function="<?php echo $this->escape($onclick); ?>"
+                                   data-id="<?php echo $item->id; ?>"
+                                   data-title="<?php echo $this->escape($item->title); ?>"
+                                   data-uri="<?php echo $this->escape(BPGalleryHelperRoute::getImageRoute($item->id,
+                                       $item->language)); ?>"
+                                   data-language="<?php echo $this->escape($lang); ?>">
+                                    <?php echo $this->escape($item->title); ?>
+                                </a>
+                                <span class="small">
+                                    <?php echo JText::sprintf('JGLOBAL_LIST_ALIAS', $this->escape($item->alias)); ?>
+                                </span>
+                                <div class="small">
+                                    <?php echo JText::_('JCATEGORY') . ': ' . $this->escape($item->category_title); ?>
+                                </div>
+                            </div>
+                        </td>
+                        <td class="small hidden-phone" style="vertical-align: middle">
+                            <?php echo $item->access_level; ?>
+                        </td>
+                        <td class="small nowrap hidden-phone" style="vertical-align: middle">
+                            <?php if ($item->language == '*'): ?>
+                                <?php echo JText::alt('JALL', 'language'); ?>
+                            <?php else: ?>
+                                <?php echo $item->language_title ? JHtml::_('image',
+                                        'mod_languages/' . $item->language_image . '.gif', $item->language_title,
+                                        ['title' => $item->language_title],
+                                        true) . '&nbsp;' . $this->escape($item->language_title) : JText::_('JUNDEFINED'); ?>
+                            <?php endif; ?>
+                        </td>
+                        <td class="hidden-phone" style="vertical-align: middle">
+                            <?php echo $item->id; ?>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
+                </tbody>
+            </table>
+        <?php endif; ?>
+
+        <input type="hidden" name="task" value=""/>
+        <input type="hidden" name="forcedLanguage"
+               value="<?php echo $app->input->get('forcedLanguage', '', 'CMD'); ?>"/>
+        <?php echo JHtml::_('form.token'); ?>
+
+    </form>
+</div>
